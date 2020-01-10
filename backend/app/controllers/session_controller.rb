98ab7fac274432afbdf98aa6f14d66dd3a3ea0c0
Mark Daniel Delgado <markpnoi26@gmail.com>
2020-01-09 18:27:01 -0800

    initial commit 2

--- backend/app/controllers/session_controller.rb
+++ backend/app/controllers/session_controller.rb
@@ -1,25 +1,23 @@
 class SessionController < ApplicationController
 
   # to be edited
-  
+
   def create
     if params[:user]
       @user = User.find_by_email(params[:user][:email])
       if @user && @user.authenticate(params[:user][:password])
         session[:user_id] = @user.id
       else
-        flash[:error] = "User Cannot be Authenticated"
-        redirect_to(new_session_path)
+        render json: {error: "User Cannot be Authenticated"}
       end
     else
-      flash[:error] = "User cannot be Authenticated"
-      redirect_to(new_session_path)
+      render json: {error: "User Cannot be Authenticated"}
     end
   end
 
   def destroy
     reset_session
-    render json: {message: "session ended"}
+    render json: { message: "session ended" }
   end
 
 end
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  const renderCondition = () => {
+    if (this.props.currentlyLoggedIn === true) {
+      return <h1>I am in the app Now </h1>
+    } else {
+      return <h1>I am the login Page </h1>
+    }
+  }
+
+  render(){
+    return (
+      {this.renderCondition()}
+    );
+  }
+
 }
 
-export default App;
+const mapStateToProps = state => {
+  return{
+    currentlyLoggedIn: state.currentlyLoggedIn
+  }
+}
+
+export default connect(mapStateToProps)(App)
